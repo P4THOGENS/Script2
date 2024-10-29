27712b9bd25c6b3325f0b187e79535befb646844
Saad <saadaas999@gmail.com>
2024-10-29 15:00:20 -0500

    Added choice 4

--- script.sh
+++ script.sh
@@ -18,12 +18,16 @@ option3()
 	ps aux
 }
 
+option4()
+{
+	
 while true; do 
 	echo "Please choose an option:"
 	echo "1. Display disk usage"
 	echo "2. Show date and time"
 	echo "3. List all running processes"
-	echo "4. Exit"
+	echo "4. Enter the app you want to open (browser, editor, music, filemanager)"
+	echo "5. Exit"
 	read -p "Enter your choice: " choice
 	
 	case $choice in
@@ -36,14 +40,37 @@ while true; do
 	3)
 		option3
 		;;
-	4)
+		
+	4)	option4
+		;;
+		echo "Enter the app you want to open (e.g., browser, editor, music, filemanager):"
+    		read app_name
+
+    		case $app_name in
+        	"browser")
+            	    xdg-open "https://www.google.com"  
+            	    ;;
+        	"editor")
+            	    code .  
+           	    ;;
+        	"music")
+           	    rhythmbox &  
+            	    ;;
+       		"filemanager")
+            	    nautilus &  
+                    ;;
+        *)
+            echo "Application not recognized. Try 'browser', 'editor', 'music', or 'filemanager'."
+            ;;
+    esac
+	5)
 		echo "Exiting..."
 		sleep 1
 		break
 		;;
 	*)
 		echo "Invalid choice, please try again."
-		;;
+		;;Enter the app you want to open (e.g., browser, editor, music):"
 	esac
 	
 	echo "Press any key to continue.."
